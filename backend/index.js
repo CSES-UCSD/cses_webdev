4c2cb6165c351653306a514cb0de9a6a5d94e9ae
SithuSoe04 <63730799+SithuSoe04@users.noreply.github.com>
2024-11-12 21:07:54 -0800

    Updated Mailchimp API Key

--- backend/index.js
+++ backend/index.js
@@ -20,20 +20,8 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-const allowedOrigins = ['http://localhost:3000', 'https://csesucsd.com'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  credentials: true
-}));
 
-// app.use(cors());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 const baseApi = '/api/v1';
